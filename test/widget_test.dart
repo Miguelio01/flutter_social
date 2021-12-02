61091daec4d3f3f6ac531628afbd93f2748d4612
Ruthford Jay <ruthford.jay@cannoncol.com>
2021-12-01 21:03:58 -0500

    Proyecto base para red social utilizando firebase con cloud_storage

---
+++ test/widget_test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social/data/model/message.dart';
+import 'package:flutter_social/domain/controller/authentication_controller.dart';
+import 'package:flutter_social/domain/controller/chat_controller.dart';
+import 'package:flutter_social/ui/widgets/chat_page.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
+
+class MockChatController extends GetxService
+    with Mock
+    implements ChatController {
+  @override
+  var messages = <Message>[].obs;
+  @override
+  Future<void> sendMsg(String text) async {
+    messages.add(Message('key', text, '001'));
+  }
+}
+
+class MockAuthenticationController extends GetxService
+    with Mock
+    implements AuthenticationController {
+  @override
+  String getUid() {
+    return '001';
+  }
+}
+
+void main() {
+  setUp(() {
+    MockAuthenticationController mockAuthenticationController =
+        MockAuthenticationController();
+    Get.put<AuthenticationController>(mockAuthenticationController);
+
+    MockChatController mockChatController = MockChatController();
+    Get.put<ChatController>(mockChatController);
+  });
+  testWidgets('Simple chat test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: ChatPage(),
+        ),
+      ),
+    );
+
+    await tester.pump();
+
+    await tester.enterText(find.byKey(const Key('MsgTextField')), 'Juan');
+
+    await tester.tap(find.byKey(const Key('sendButton')));
+
+    await tester.pump();
+
+    expect(find.text('Juan'), findsOneWidget);
+  });
+}
